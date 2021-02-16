1e240a7633b36a225a00af7a27366ef895810d26
y4kw <y4kw@math.sci.hokudai.ac.jp>
2021-02-16 12:07:42 +0900

    1205

--- app/src/main/java/com/example/gv/MainActivity.java
+++ app/src/main/java/com/example/gv/MainActivity.java
@@ -1,56 +1,94 @@
 package com.example.gv;
 
+import android.app.Activity;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.widget.LinearLayout;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.text.SpannableStringBuilder;
 
-import android.view.Menu;
-import android.view.MenuItem;
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener{
+//public class Test02_01 extends Activity implements OnClickListener{
 
+    private final int FP = ViewGroup.LayoutParams.FILL_PARENT;
+    private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+    private EditText textUrl;
+    private Button buttonGo;
+    private WebView webview;
+
+    @Override protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(linearLayout);
+
+
+        LinearLayout ctlLinearLayout = new LinearLayout(this);
+        ctlLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        textUrl = new EditText(this);
+        textUrl.setText("http://");
+
+        buttonGo = new Button(this);
+        buttonGo.setText("Go");
+        buttonGo.setOnClickListener(this);
+
+        ctlLinearLayout.addView(buttonGo, createParam(WC, WC));
+        ctlLinearLayout.addView(textUrl, createParam(FP, WC));
+
+/*
+        webview = new WebView(this);
+        //webview.loadUrl("http://www.yahoo.co.jp/");
+        webview.loadUrl("https://www.data.jma.go.jp/fcd/yoho/data/jishin/kaisetsu_tanki_latest.pdf");
+*/
+
+        //       You can use Google PDF Viewer to read your pdf online:
+/*
+        //WebView webview = (WebView) findViewById(R.id.webview);
+        webview = new WebView(this);
+        webview.getSettings().setJavaScriptEnabled(true);
+        String pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
+        //webview.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
+        webview.loadUrl("https://www.data.jma.go.jp/fcd/yoho/data/jishin/kaisetsu_tanki_latest.pdf");
+
+ */
+
+        webview = new WebView(this);
+        String pdfUrl = "https://www.data.jma.go.jp/fcd/yoho/data/jishin/kaisetsu_tanki_latest.pdf";
+        String url = "http://docs.google.com/gview?embedded=true&url=" + pdfUrl;
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(url);
+
+
+        linearLayout.addView(ctlLinearLayout, createParam(FP, WC));
+        linearLayout.addView(webview, createParam(WC, WC));
+    }
+
+    private LinearLayout.LayoutParams createParam(int w, int h){
+        return new LinearLayout.LayoutParams(w, h);
+    }
+
+    public void onClick(View v) {
+        SpannableStringBuilder url = (SpannableStringBuilder)textUrl.getText();
+        webview.loadUrl(url.toString());
+    }
+}
+
+/*
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
+ */
